d80edaa0bdf4245fe81beaef0021c42c462cac66
Adil <christie_m@mail.ru>
2022-09-18 17:14:24 +0300

    Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.

---
+++ Task33/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+Console.WriteLine("Введите ваше число");
+int num = Convert.ToInt32(Console.ReadLine());
+int count = num * -1;
+
+while (count <= num)
+{
+    Console.Write($"{count} ");
+    count++;
+}
