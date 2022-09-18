b041017fe5e8ef60cb15ce8996918bc4c5344b71
Adil <christie_m@mail.ru>
2022-09-18 17:03:46 +0300

    Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.

---
+++ Task26/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 60. Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. 
+// Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+
+Console.Clear();
+
+void PrintMatrix(int[,,] matrix)                             
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                Console.Write($"{matrix[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+int newRandomValue(int[,,] matrix)
+{
+    int randomValue = new Random().Next(10, 100);
+    bool numExists = false;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int k = 0; k < matrix.GetLength(2); k++)
+            {
+                if (matrix[i, j, k] != randomValue) numExists = false;
+                else
+                {
+                    numExists = true;
+                    return newRandomValue(matrix);
+                }
+            }
+        }
+    }
+    if (!numExists) return randomValue;
+    else return newRandomValue(matrix);
+}
+
+var rand = new Random();
+int x = rand.Next(2, 5);
+int y = rand.Next(2, 5);
+int z = rand.Next(2, 5);
+int[,,] nums = new int[x, y, z];
+
+for (int i = 0; i < nums.GetLength(0); i++)
+{
+    for (int j = 0; j < nums.GetLength(1); j++)
+    {
+        for (int k = 0; k < nums.GetLength(2); k++)
+        {
+            nums[i, j, k] = newRandomValue(nums);
+        }
+    }
+}
+PrintMatrix(nums);
