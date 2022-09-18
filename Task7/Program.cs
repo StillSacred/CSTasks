18f8ac685cc171741520d81b04635ba2234f0dca
Adil <christie_m@mail.ru>
2022-09-18 13:12:37 +0300

    Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.

---
+++ Task7/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num == 1) Console.WriteLine("Понедельник - рабочий день");
+if (num == 2) Console.WriteLine("Вторник - рабочий день");
+if (num == 3) Console.WriteLine("Среда - рабочий день");
+if (num == 4) Console.WriteLine("Четверг - рабочий день");
+if (num == 5) Console.WriteLine("Пятница - рабочий день");
+if (num == 6) Console.WriteLine("Суббота - выходной");
+if (num == 7) Console.WriteLine("Воскресенье - выходной");
