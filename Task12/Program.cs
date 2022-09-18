3f6ff4511938d524bcccc37db27ce7c4931710d5
Adil <christie_m@mail.ru>
2022-09-18 13:41:58 +0300

    Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.

---
+++ Task12/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+void SumOfNum(int num)
+{
+    int sum = 0;
+    while (num != 0)
+    {
+        sum = sum + num % 10;
+        num = num / 10;
+    }
+    Console.WriteLine(sum);
+}
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+SumOfNum(num);
