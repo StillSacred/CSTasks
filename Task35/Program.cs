3503afc58c227f918dab42342138f163859b5b1f
Adil <christie_m@mail.ru>
2022-09-18 17:17:30 +0300

    случайное число, программа выдает максимальную цифру этого числа

---
+++ Task35/Program.cs
@@ -0,0 +1,17 @@
+﻿// случайное число, программа выдает максимальную цифру этого числа
+
+Console.Clear();
+
+int num = new Random().Next(10, 100);
+Console.WriteLine(num);
+int num2 = num / 10;
+int num3 = num % 10;
+
+if (num2 > num3)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    Console.WriteLine(num3);
+}
