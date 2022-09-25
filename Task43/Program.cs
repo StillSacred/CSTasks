0a2ba11ac7b00a351f792c60e32f62efbd7a2a16
Adil <christie_m@mail.ru>
2022-09-25 11:36:31 +0300

    Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.

---
+++ Task43/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+
+Console.WriteLine("Введите N ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+
+while (count <= n)
+{
+    Console.Write($"{Math.Pow(count, 2)} ");
+    count++;
+}
