dcca29a9193785f9a4736720ef7fb4f72920b88a
Adil <christie_m@mail.ru>
2022-09-25 11:59:58 +0300

    Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.

---
+++ Task59/Program.cs
@@ -0,0 +1,44 @@
+﻿// Задача 46: Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+void FillMatrix(int[,] matrix)                              // Заполнение массива случайными целыми числами
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(2, 16);
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"[{matrix[i, j]}] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Задайте количество строк массива");      // Задаем размеры матрицы и заполняем случайными числами
+int rows = Convert.ToInt32(Console.ReadLine());
+while (rows < 2)
+{
+    Console.WriteLine("Количество строк не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    rows = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Задайте количество столбцов массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+while (cols < 2)
+{
+    Console.WriteLine("Количество столбцов не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    cols = Convert.ToInt32(Console.ReadLine());
+}
+int[,] numbers = new int[rows, cols];
+FillMatrix(numbers);
+PrintMatrix(numbers);
