bf2e8b6e3a42660bae90964dc3080846f9893ebd
Adil <christie_m@mail.ru>
2022-09-25 12:10:20 +0300

    Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.

---
+++ Task65/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 63: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от 1 до N.
+
+void ShowIntegersFromNTo1(int n)
+{
+    if (n != 1)
+    {
+        ShowIntegersFromNTo1(n - 1);
+    }
+    Console.Write($"{n} ");
+}
+
+int n = 5;
+ShowIntegersFromNTo1(n);
