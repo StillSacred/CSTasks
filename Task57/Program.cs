5cb0d35240aa158832cccddb6909442e02b397b9
Adil <christie_m@mail.ru>
2022-09-25 11:57:39 +0300

    Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.

---
+++ Task57/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = m+n. Выведите полученный массив на экран.
+
+Console.Clear();
+
+void FillMatrix(int[,] matrix)                              // Заполнение массива по формуле: Aₘₙ = m+n
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"[{matrix[i, j]}] ");
+        }
+        Console.WriteLine();
+    }
+}
+
+Console.WriteLine("Задайте количество строк массива");
+int rows = Convert.ToInt32(Console.ReadLine());
+while (rows < 2)
+{
+    Console.WriteLine("Количество строк не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    rows = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Задайте количество столбцов массива");
+int cols = Convert.ToInt32(Console.ReadLine());
+while (cols < 2)
+{
+    Console.WriteLine("Количество столбцов не может быть меньше 2");
+    Console.WriteLine("Повторите ввод значения");
+    cols = Convert.ToInt32(Console.ReadLine());
+}
+int[,] numbers = new int[rows, cols];
+FillMatrix(numbers);
+PrintMatrix(numbers);
