5fc66cb3977848ef98da60fcdcd072f9ca065036
Adil <christie_m@mail.ru>
2022-09-25 11:47:49 +0300

    Напишите программу замены элементов массива: поменять положительные элементы на соотвествуеющие отрицательные, и наоборот.

---
+++ Task50/Program.cs
@@ -0,0 +1,35 @@
+﻿// Напишите программу замены элементов массива: поменять положительные элементы на соотвествуеющие отрицательные, и наоборот.
+
+Console.Clear();
+
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(-50, 51);
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+void ChangePosToNeg(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] *= -1;
+    }
+}
+
+int[] numbers = new int[10];
+
+FillArray(numbers);
+PrintArray(numbers);
+Console.WriteLine();
+ChangePosToNeg(numbers);
+PrintArray(numbers);
