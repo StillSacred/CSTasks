23be9757dbe194e5170c99b83fefa6c3aa411198
Adil <christie_m@mail.ru>
2022-09-25 12:11:10 +0300

    Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.

---
+++ Task66/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 65: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+
+void NumbersFormMToN(int m, int n)
+{
+    if (n != m)
+    {
+       NumbersFormMToN(m, n - 1);
+    }
+    Console.Write($"{n} ");
+}
+
+int a = 4;
+int b = 20;
+NumbersFormMToN(a, b);
