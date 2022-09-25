a2d7fce8cec05926254075713b02b509e7c82558
Adil <christie_m@mail.ru>
2022-09-25 11:44:31 +0300

    Напишите программу, которая создаст массив и заполнит его рандомными значениями 0 и 1

---
+++ Task48/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая создаст массив и заполнит его рандомными значениями 0 и 1
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 2);
+        Console.Write($"{array[i]} ");
+    }
+}
+Console.WriteLine("Введите длину массива");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+FillArray(array);
